3804e0d092c457f59eef351c10c34aa82e7618c0
Tilmann Böhme <dev@tilmannb.de>
2021-07-15 12:52:10 +0000

    💄 change messaging on contact

--- webapp/web/src/pages/ContactPage/ContactPage.tsx
+++ webapp/web/src/pages/ContactPage/ContactPage.tsx
@@ -1,4 +1,3 @@
-import { RadioGroup } from '@headlessui/react'
 import { useState } from 'react'
 
 const settings = [
@@ -94,82 +93,14 @@ const ContactPage = () => {
                         Wir sind fast soweit.
                       </h2>
                       <p className="mt-6 mx-auto max-w-2xl text-lg text-indigo-200">
-                        Danke für Ihr Interesse. Momentan arbeiten wir mit
-                        Hochdruck daran, die passenden Angebote für eine Online
-                        Energieberatung zur Verfügung zu stellen.
+                        Danke für ihr Interesse. Wir arbeiten mit Hochdruck
+                        daran, die passenden Angebote für eine
+                        Online-Energieberatung zur Verfügung zu stellen. <br />
+                        Gerne kontaktieren wir sie, um ein kurzes Gespräch
+                        bezüglich ihrer Erwartungen, zu vereinbaren.
                       </p>
                     </div>
                     <form action="#" className="mt-12 sm:mx-auto sm:max-w-lg">
-                      <RadioGroup value={selected} onChange={setSelected}>
-                        <RadioGroup.Label className="sr-only">
-                          Privacy setting
-                        </RadioGroup.Label>
-                        <div className="bg-white rounded-md -space-y-px">
-                          {settings.map((setting, settingIdx) => (
-                            <RadioGroup.Option
-                              key={setting.name}
-                              value={setting}
-                              className={({ checked }) =>
-                                classNames(
-                                  settingIdx === 0
-                                    ? 'rounded-tl-md rounded-tr-md'
-                                    : '',
-                                  settingIdx === settings.length - 1
-                                    ? 'rounded-bl-md rounded-br-md'
-                                    : '',
-                                  checked
-                                    ? 'bg-indigo-50 border-indigo-200 z-10'
-                                    : 'border-gray-200',
-                                  'relative border p-4 flex cursor-pointer focus:outline-none'
-                                )
-                              }
-                            >
-                              {({ active, checked }) => (
-                                <>
-                                  <span
-                                    className={classNames(
-                                      checked
-                                        ? 'bg-indigo-600 border-transparent'
-                                        : 'bg-white border-gray-300',
-                                      active
-                                        ? 'ring-2 ring-offset-2 ring-indigo-500'
-                                        : '',
-                                      'h-4 w-4 mt-0.5 cursor-pointer rounded-full border flex items-center justify-center'
-                                    )}
-                                    aria-hidden="true"
-                                  >
-                                    <span className="rounded-full bg-white w-1.5 h-1.5" />
-                                  </span>
-                                  <div className="ml-3 flex flex-col">
-                                    <RadioGroup.Label
-                                      as="span"
-                                      className={classNames(
-                                        checked
-                                          ? 'text-indigo-900'
-                                          : 'text-gray-900',
-                                        'block text-sm font-medium'
-                                      )}
-                                    >
-                                      {setting.name}
-                                    </RadioGroup.Label>
-                                    <RadioGroup.Description
-                                      as="span"
-                                      className={classNames(
-                                        checked
-                                          ? 'text-indigo-700'
-                                          : 'text-gray-500',
-                                        'block text-sm'
-                                      )}
-                                    >
-                                      {setting.description}
-                                    </RadioGroup.Description>
-                                  </div>
-                                </>
-                              )}
-                            </RadioGroup.Option>
-                          ))}
-                        </div>
-                      </RadioGroup>
                       <div className="sm:flex py-8">
                         <div className="min-w-0 flex-1">
                           <label htmlFor="cta-email" className="sr-only">
@@ -187,10 +118,14 @@ const ContactPage = () => {
                             type="submit"
                             className="block w-full rounded-md border border-transparent px-5 py-3 bg-indigo-500 text-base font-medium text-white shadow hover:bg-indigo-400 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-indigo-600 sm:px-10"
                           >
-                            Eintragen!
+                            Gespräch führen
                           </button>
                         </div>
                       </div>
+                      <span className="text-white text-xs">
+                        Ihre E-Mailadresse wird ausschließlich zur
+                        Kontaktaufnahme in Bezug auf unser Produkt verwendet.
+                      </span>
                     </form>
                   </div>
                 </div>
