6cd84e34c992a2b303409da387a05f763895fff5
Tilmann Böhme <dev@tilmannb.de>
2021-09-13 12:36:48 +0000

    📝 use storybook for FlatCard

--- webapp/web/src/components/Dashboard/FlatCard/FlatCard.stories.tsx
+++ webapp/web/src/components/Dashboard/FlatCard/FlatCard.stories.tsx
@@ -1,7 +1,15 @@
 import FlatCard from './FlatCard'
 
 export const generated = () => {
-  return <FlatCard />
+  return (
+    <FlatCard
+      flat={{
+        id: 1,
+        squareMeters: 212,
+        zipCode: 12345,
+      }}
+    />
+  )
 }
 
 export default { title: 'Components/FlatCard' }
