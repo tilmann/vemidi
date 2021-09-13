a62886e1eb34bd10954bbc31485d35524f8a8085
Tilmann Böhme <dev@tilmannb.de>
2021-09-13 13:17:12 +0000

    📝 add story for one and multiple flatCards

--- webapp/web/src/components/FlatsCardsCell/FlatsCardsCell.mock.ts
+++ webapp/web/src/components/FlatsCardsCell/FlatsCardsCell.mock.ts
@@ -1,4 +1,14 @@
 // Define your own mock data here:
+
+const flatCards = [
+    { id: 42, zipCode: 12345, squareMeters: 11 }, 
+    { id: 43, zipCode: 12346, squareMeters: 22 }, 
+    { id: 44, zipCode: 12347, squareMeters: 33 }]
+
 export const standard = (/* vars, { ctx, req } */) => ({
-  flatsCards: [{ id: 42 }, { id: 43 }, { id: 44 }],
+  flatsCards: flatCards,
+})
+
+export const oneCard = (/* vars, { ctx, req } */) => ({
+  flatsCards: [flatCards[0]],
 })
--- webapp/web/src/components/FlatsCardsCell/FlatsCardsCell.stories.tsx
+++ webapp/web/src/components/FlatsCardsCell/FlatsCardsCell.stories.tsx
@@ -1,5 +1,5 @@
-import { Loading, Empty, Failure, Success } from './FlatsCardsCell'
-import { standard } from './FlatsCardsCell.mock'
+import { Empty, Failure, Loading, Success } from './FlatsCardsCell'
+import { oneCard, standard } from './FlatsCardsCell.mock'
 
 export const loading = () => {
   return Loading ? <Loading /> : null
@@ -13,7 +13,11 @@ export const failure = () => {
   return Failure ? <Failure error={new Error('Oh no')} /> : null
 }
 
-export const success = () => {
+export const success1 = () => {
+  return Success ? <Success {...oneCard()} /> : null
+}
+
+export const successMultiple = () => {
   return Success ? <Success {...standard()} /> : null
 }
 
